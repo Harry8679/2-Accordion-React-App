3a5d3a748e92d3af80efcf62d9fffba3bed8d152
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-14 13:30:33 +0100

    Accordion - JSX

---
+++ src/components/faq/Faq.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaBook } from "react-icons/fa";
+
+const Faq = () => {
+  return (
+    <section className='faq-sec'>
+        <div className="container faq">
+            <div className="title --center-all">
+                <FaBook size={30} color='orangered' />
+                <h2 className='--mb2'>FAQS</h2>
+                <p className='--text-small'>
+                    Fréquemment traduit en français par Foire Aux Questions, l'acronyme FAQ fait référence à l'expression anglaise Frequently Asked Questions. 
+                    Il s'agit en fait d'un support d'information qui regroupe, sur une même page Web, les questions les plus souvent posées par des internautes.
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Faq
---
+++ src/components/faq/data.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+        id: 1,
+        title: 'With what programming language can I do everything?',
+        answer: 'The most common language for doing everything is JavaScript because you can do web application (Front and Back), mobile application, software application and even video games'
+    },
+    {
+        id: 2,
+        title: 'Is it difficult to learn JavaScript ?',
+        answer: 'JavaScript is an easy language to learn, it just takes regular work, perseverance and discipline.'
+    },
+    {
+        id: 3,
+        title: 'What are the Web Frameworks associated with JavaScript ?',
+        answer: 'The main web frameworks associated with JavaScript are three: Angular, React and Vue.'
+    },
+    {
+        id: 4,
+        title: 'What is the JavaScript technology for programming on the Back side ?',
+        answer: 'The JavaScript technology on the Back side is NodeJS which is an runtime environment.'
+    }
+];
